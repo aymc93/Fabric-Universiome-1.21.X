b75d44c973a9d501c045f22d8eb9d879774fcd7c
aymeric <aclaudio@iut.univ-paris8.fr>
2024-12-29 15:37:37 +0100

    Initial commit

---
+++ src/main/java/net/ironpnh/universiome/Universiome.java
@@ -0,0 +1,16 @@
+package net.ironpnh.universiome;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Universiome implements ModInitializer {
+	public static final String MOD_ID = "universiome";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ironpnh/universiome/UniversiomeClient.java
@@ -0,0 +1,10 @@
+package net.ironpnh.universiome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UniversiomeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
